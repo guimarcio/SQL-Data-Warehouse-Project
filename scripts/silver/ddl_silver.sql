7070255586e63c80c228e82f0feba81269844bfb
Guilherme Márcio <162584234+guimarcio@users.noreply.github.com>
2026-06-16 09:26:11 -0300

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -1,5 +1,6 @@
--- Creating tables in silver schema using Data Definition Language (DDL) (used notepad++ for fast replacement)
-
+/* 	Creating tables in silver schema using Data Definition Language (DDL) (used notepad++ for fast replacement)
+	Here are created all the tables with the transformed data from the previous layer (bronze layer)
+*/
 IF OBJECT_ID ('silver.crm_cust_info' , 'U') IS NOT NULL 
 	DROP TABLE silver.crm_cust_info;
 CREATE TABLE silver.crm_cust_info (
